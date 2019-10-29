d8d83f9b38386beced2dbfdfb629da07aecab537
Nathan GUIGUI <nathan.guigui@epitech.eu>
2019-10-30 00:37:04 +0100

    :sparkles: add field on user in backend for api token

--- back-end/src/resolvers/User.ts
+++ back-end/src/resolvers/User.ts
@@ -3,6 +3,6 @@ import { prismaObjectType } from 'nexus-prisma'
 export const User = prismaObjectType({
   name: 'User',
   definition(t) {
-    t.prismaFields(['id', 'email', 'name', "surname", "address", "phone", "type"])
+    t.prismaFields(['id', 'email', 'name', "surname", "address", "phone", "type", "refreshTime", "backgroundImage", "sidebarDisabled", "googleToken", "intraToken"])
   },
 })
